cea4b8ac623aa1b6b2dc96f3ac405b45df01881e
Kasper Mikiewicz <IderedPL@gmail.com>
2020-10-07 07:50:26 +0200

    chore: bump dependencies

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
     }
   },
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended'
+    'plugin:@typescript-eslint/recommended'
   ],
   overrides: [{
     files: ['*.ts', '*.tsx'],
