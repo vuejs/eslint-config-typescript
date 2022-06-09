8c3628cc43ce93c197d1f6b749712d2c0b2ebe4e
Haoqun Jiang <haoqunjiang@gmail.com>
2022-06-09 16:49:21 +0800

    feat: use multiple parsers for more precise check
    This feature was addded in https://github.com/vuejs/vue-eslint-parser/pull/116

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
+
   // Prerequisite `eslint-plugin-vue`, being extended, sets
   // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
   // in turn delegates to the parser, specified in `parserOptions.parser`:
   // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
   parserOptions: {
-    parser: require.resolve('@typescript-eslint/parser'),
+    parser: {
+      'js': 'espree',
+      'jsx': 'espree',
+
+      'ts': require.resolve('@typescript-eslint/parser'),
+      'tsx': require.resolve('@typescript-eslint/parser'),
+
+      // Leave the template parser unspecified, so that it could be determined by `<script lang="...">`
+    },
     extraFileExtensions: ['.vue'],
     ecmaFeatures: {
       jsx: true
     }
   },
+
   extends: [
     'plugin:@typescript-eslint/eslint-recommended'
   ],
+
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
