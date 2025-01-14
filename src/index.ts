75bae41733ae37d4086c01ed636c167f904df126
Haoqun Jiang <haoqunjiang@gmail.com>
2025-01-14 18:08:21 +0800

    chore: comment about why not using re-export [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export { default as createConfig } from './createConfig'
 export { default } from './createConfig'
 
 import { defineConfigWithVueTs } from './utilities'
+// Somehow re-export can't preserve the deprecated comment to the generated d.ts file
 /**
  * @deprecated `defineConfig` is renamed to `defineConfigWithVueTs` in 14.3.0
  */
