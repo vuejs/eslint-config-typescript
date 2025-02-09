98f56ad42259c15d8ef71ea8d1db2e47c6d9c475
Haoqun Jiang <haoqunjiang@gmail.com>
2025-02-09 20:56:01 +0800

    fix: proper support for `extends` field, fixes #136 (#143)

---
+++ examples/disable-type-checked-for-yml/eslint.config.js
@@ -0,0 +1,42 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginCypress from 'eslint-plugin-cypress/flat'
+import pluginYml from 'eslint-plugin-yml';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue,yml,yaml}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommendedTypeChecked,
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+
+  {
+    ...pluginCypress.configs.recommended,
+    files: [
+      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
+  skipFormatting,
+
+  ...pluginYml.configs['flat/recommended'],
+
+  {
+    files: ["**/*.yml", "**/*.yaml"],
+    extends: [vueTsConfigs.disableTypeChecked],
+  },
+)
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,7 @@
 import process from 'node:process'
-import * as tseslint from 'typescript-eslint'
+import tseslint from 'typescript-eslint'
+import type { TSESLint } from '@typescript-eslint/utils'
+
 import { TsEslintConfigForVue } from './configs'
 import groupVueFiles from './groupVueFiles'
 import {
@@ -8,16 +10,18 @@ import {
   createSkipTypeCheckingConfigs,
   createTypeCheckingConfigs,
 } from './internals'
-
 import type { ScriptLang } from './internals'
 
-type ConfigArray = ReturnType<typeof tseslint.config>
-type Rules = NonNullable<ConfigArray[number]['rules']>
+import { omit, pipe, partition } from './fpHelpers'
 
-type ConfigObjectOrPlaceholder = ConfigArray[number] | TsEslintConfigForVue
-type InfiniteDepthConfigWithVueSupport =
-  | ConfigObjectOrPlaceholder
-  | InfiniteDepthConfigWithVueSupport[]
+type ConfigItem = TSESLint.FlatConfig.Config
+type InfiniteDepthConfigWithExtendsAndVueSupport =
+  | TsEslintConfigForVue
+  | ConfigItemWithExtendsAndVueSupport
+  | InfiniteDepthConfigWithExtendsAndVueSupport[]
+interface ConfigItemWithExtendsAndVueSupport extends ConfigItem {
+  extends?: InfiniteDepthConfigWithExtendsAndVueSupport[]
+}
 
 export type ProjectOptions = {
   /**
@@ -62,51 +66,111 @@ export function configureVueProject(userOptions: ProjectOptions): void {
   }
 }
 
+// The *Raw* types are those with placeholders not yet resolved.
+type RawConfigItemWithExtends =
+  | ConfigItemWithExtendsAndVueSupport
+  | TsEslintConfigForVue
+type RawConfigItem = ConfigItem | TsEslintConfigForVue
+
 export function defineConfigWithVueTs(
-  ...configs: InfiniteDepthConfigWithVueSupport[]
-): ConfigArray {
-  // @ts-ignore
-  const flattenedConfigs: ConfigObjectOrPlaceholder[] = configs.flat(Infinity)
-
-  const reorderedConfigs = insertAndReorderConfigs(flattenedConfigs)
-
-  const normalizedConfigs = reorderedConfigs.map(config =>
-    config instanceof TsEslintConfigForVue ? config.toConfigArray() : config,
+  ...configs: InfiniteDepthConfigWithExtendsAndVueSupport[]
+): ConfigItem[] {
+  return pipe(
+    configs,
+    flattenConfigs,
+    insertAndReorderConfigs,
+    resolveVueTsConfigs,
+    tseslint.config,  // this might not be necessary, but it doesn't hurt to keep it
   )
-
-  return tseslint.config(...normalizedConfigs)
 }
 
-// This function reorders the config array to make sure it satisfies the following layout:
-//
-// [FIRST-EXTENDED-CONFIG]
-// ...
-// [LAST-EXTENDED-CONFIG]
-//
-// [BASIC SETUP]
-//   pluginVue.configs['flat/base'],
-//   '@vue/typescript/setup'
-//
-// [ALL-OTHER-TYPE-AWARE-RULES-CONFIGURED-BY-USERS]
-//
-// [ERROR PREVENTION & PERFORMANCE OPTIMIZATION]
-//   '@vue/typescript/skip-type-checking-for-js-files'
-//   '@vue/typescript/skip-type-checking-for-vue-files-without-ts'
-//
-// [ONLY REQUIRED WHEN ONE-OR-MORE TYPE-AWARE RULES ARE TURNED-ON]
-//   '@vue/typescript/default-project-service-for-ts-files'
-//   '@vue/typescript/default-project-service-for-vue-files'
-//   '@vue/typescript/type-aware-rules-in-conflit-with-vue'
+function flattenConfigs(
+  configs: InfiniteDepthConfigWithExtendsAndVueSupport[],
+): RawConfigItem[] {
+  // Be careful that our TS types don't guarantee that `extends` is removed from the final config
+  // Modified from
+  // https://github.com/typescript-eslint/typescript-eslint/blob/d30a497ef470b5a06ca0a5dde9543b6e00c87a5f/packages/typescript-eslint/src/config-helper.ts#L98-L143
+  // No handling of undefined for now for simplicity
+
+  // @ts-expect-error -- intentionally an infinite type
+  return (configs.flat(Infinity) as RawConfigItemWithExtends[]).flatMap(
+    (c: RawConfigItemWithExtends): RawConfigItem | RawConfigItem[] => {
+      if (c instanceof TsEslintConfigForVue) {
+        return c
+      }
+
+      const { extends: extendsArray, ...restOfConfig } = c
+      if (extendsArray == null || extendsArray.length === 0) {
+        return restOfConfig
+      }
+
+      const flattenedExtends: RawConfigItem[] = extendsArray.flatMap(
+        configToExtend =>
+          Array.isArray(configToExtend)
+            ? flattenConfigs(configToExtend)
+            : [configToExtend],
+      )
+
+      return [
+        ...flattenedExtends.map((extension: RawConfigItem) => {
+          if (extension instanceof TsEslintConfigForVue) {
+            return extension.asExtendedWith(restOfConfig)
+          } else {
+            const name = [restOfConfig.name, extension.name]
+              .filter(Boolean)
+              .join('__')
+            return {
+              ...extension,
+              ...(restOfConfig.files && { files: restOfConfig.files }),
+              ...(restOfConfig.ignores && { ignores: restOfConfig.ignores }),
+              ...(name && { name }),
+            }
+          }
+        }),
+
+        // If restOfConfig contains nothing but `ignores`/`name`, we shouldn't return it
+        // Because that would make it a global `ignores` config, which is not what we want
+        ...(Object.keys(omit(restOfConfig, ['ignores', 'name'])).length > 0
+          ? [restOfConfig]
+          : []),
+      ]
+    },
+  )
+}
+
+function resolveVueTsConfigs(configs: RawConfigItem[]): ConfigItem[] {
+  return configs.flatMap(config =>
+    config instanceof TsEslintConfigForVue ? config.toConfigArray() : config,
+  )
+}
 
 type ExtractedConfig = {
   files?: (string | string[])[]
-  rules: Rules
+  rules: NonNullable<ConfigItem['rules']>
 }
 const userTypeAwareConfigs: ExtractedConfig[] = []
 
-function insertAndReorderConfigs(
-  configs: ConfigObjectOrPlaceholder[],
-): ConfigObjectOrPlaceholder[] {
+/**
+ * This function reorders the config array to make sure it satisfies the following layout:
+ *
+ * ```
+ * [FIRST-EXTENDED-CONFIG]
+ * ...
+ * [LAST-EXTENDED-CONFIG]
+ * [BASIC SETUP]
+ *   pluginVue.configs['flat/base'],
+ *   '@vue/typescript/setup'
+ * [ALL-OTHER-TYPE-AWARE-RULES-CONFIGURED-BY-USERS]
+ * [ERROR PREVENTION & PERFORMANCE OPTIMIZATION]
+ *   '@vue/typescript/skip-type-checking-for-js-files'
+ *   '@vue/typescript/skip-type-checking-for-vue-files-without-ts'
+ * [ONLY REQUIRED WHEN ONE-OR-MORE TYPE-AWARE-RULES ARE TURNED-ON]
+ *   '@vue/typescript/default-project-service-for-ts-files'
+ *   '@vue/typescript/default-project-service-for-vue-files'
+ *   '@vue/typescript/type-aware-rules-in-conflit-with-vue'
+ * ```
+ */
+function insertAndReorderConfigs(configs: RawConfigItem[]): RawConfigItem[] {
   const lastExtendedConfigIndex = configs.findLastIndex(
     config => config instanceof TsEslintConfigForVue,
   )
@@ -147,9 +211,7 @@ function insertAndReorderConfigs(
   ]
 }
 
-function extractTypeAwareRules(
-  config: ConfigObjectOrPlaceholder,
-): ConfigObjectOrPlaceholder {
+function extractTypeAwareRules(config: RawConfigItem): RawConfigItem {
   if (config instanceof TsEslintConfigForVue) {
     return config
   }
@@ -185,20 +247,4 @@ const rulesRequiringTypeInformation = new Set(
 )
 function doesRuleRequireTypeInformation(ruleName: string): boolean {
   return rulesRequiringTypeInformation.has(ruleName)
-}
-
-function partition<T>(
-  array: T[],
-  predicate: (element: T) => boolean,
-): [T[], T[]] {
-  const truthy: T[] = []
-  const falsy: T[] = []
-  for (const element of array) {
-    if (predicate(element)) {
-      truthy.push(element)
-    } else {
-      falsy.push(element)
-    }
-  }
-  return [truthy, falsy]
 }
