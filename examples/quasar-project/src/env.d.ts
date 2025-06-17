3d6b82c6256f6e632342d47361dc0beb449fc7c0
Haoqun Jiang <haoqunjiang@gmail.com>
2025-06-17 17:00:28 +0800

    feat: `allowComponentTypeUnsafety` option to configure the strictness of `no-unsafe-*` rules

---
+++ examples/quasar-project/src/env.d.ts
@@ -0,0 +1,15 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
+    VUE_ROUTER_BASE: string | undefined;
+  }
+}
+
+import type { Quasar } from 'quasar';
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $q: Quasar;
+  }
+}
