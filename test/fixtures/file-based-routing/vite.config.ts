ad48e2d8182f56900d4923fbd7c5201d07b55e2f
Haoqun Jiang <github@haoqun.me>
2026-02-17 13:51:34 +0900

    chore: migrate from unplugin-vue-router to vue-router 5

--- test/fixtures/file-based-routing/vite.config.ts
+++ test/fixtures/file-based-routing/vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vueRouter from 'unplugin-vue-router/vite'
+import vueRouter from 'vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
